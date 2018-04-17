c9bd15cad4716af0d579465f35f8f4156946b246
James Smith <smittyprz94@gmail.com>
2018-04-17 19:34:15 -0400

    Fully working tSNE moving visualization, will use more values in order to make better visualization

--- Jummy/Use_Model.py
+++ Jummy/Use_Model.py
@@ -1,6 +1,5 @@
 """Yeah."""
-import time
-
+from pathlib import Path
 from sklearn.manifold import TSNE
 from torch.autograd import Variable
 import torch
@@ -8,7 +7,11 @@ import loadin
 from EMG_RNN import Net
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+import mpl_toolkits.mplot3d.axes3d as p3
+import matplotlib.animation as animation
+import pickle
+import scipy.interpolate as sp
+import time
 
 
 def evaluate(data_tensor):
@@ -34,40 +37,110 @@ def making_tensor(data, chn):
     return tensor
 
 
+def interpolation(input_dict, hidden_list, int_amount):
+    """Create Interpolation frames for tsne."""
+    output_dict = {}
+    int_val_list = []
+    range_list = []
+    set_size = input_dict[str(hidden_list[0])].shape[0]
+    for ins in range(set_size):
+        points = []
+        for hl in hidden_list:
+            data = input_dict[str(hl)][ins, :]
+            points.append(data)
+        point_set = np.stack(points, axis=1)
+        range_list.append(point_set)
+        int_val_list.append(sp.splprep(point_set, k=point_set.shape[1] - 1))
+    for step, hl in enumerate(hidden_list):
+        for int_val in range(int_amount):
+            val_list = []
+            ran = (step * (int_amount)) + int_val
+            char_in = str(ran)
+            if int_val == -1:
+                output_dict[char_in] = input_dict[str(hl)]
+            elif True:
+                for ins in range(set_size):
+                    move = np.linspace(0, 1.05, len(hidden_list) * int_amount)
+                    val = sp.splev(move[ran], int_val_list[ins][0])
+                    val = [float(x) for x in val]
+                    val_list.append(np.array(val))
+                data_fill = np.stack(val_list, axis=0)
+                output_dict[char_in] = data_fill
+    return output_dict
+
+
+def update(move, input, ob, hs, int_amount):
+    """Update Moving animation."""
+    in_ = str(move)
+
+    if move % int_amount == 0:
+        val = (move + 1) / int_amount
+        lnum = str(hs[int(val)])
+        ax.set_title('tSNE of RNN with ' + lnum + ' size for hidden layer', y=1)
+    ob._offsets3d = (input[in_][:, 0], input[in_][:, 1], input[in_][:, 2])
+    return ob,
+
+
 if __name__ == '__main__':
-    time_start = time.time()
-    tsne = TSNE(n_components=3, verbose=1, perplexity=100, n_iter=300)
-
-    rnn = torch.load('Trained_Model/trained_model.out')
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-
+    overwrite = False
     labels = ['cyl', 'hook', 'tip', 'palm', 'spher', 'lat']
-    training_set, test_set, full_set = loadin.main()
-    chn = full_set.shape[2]
+    hidden_list = [32, 64, 128, 256]
 
-    for_tsne = []
-    label_list = []
+    file_des = 'Visual Data/tsne_data.pickle'
+    # print(not Path(file_des))
+    if overwrite:
+        tsne = TSNE(n_components=3, verbose=1, perplexity=100, n_iter=300)
 
-    for label in range(len(labels)):
-        for per in range(full_set.shape[1]):
-            for ex in range(full_set.shape[3]):
-                data = full_set[label, per, :, ex, :]
-                input_data = Variable(making_tensor(data, chn))
-                output = evaluate(input_data)
-                for_tsne.append(output.data[0].numpy())
-                label_list.append(label)
-    tsne_input = np.stack(for_tsne, axis=0)
-    # tsne_input = tsne_input / np.mean(tsne_input)
-    label_color = np.array(label_list)
-    tsne_embedded = tsne.fit_transform(tsne_input)
-    print(tsne_embedded.shape)
-    ax.scatter(tsne_embedded[:, 0], tsne_embedded[:, 1], tsne_embedded[:, 2], c=label_color)
-    ax = plt.gca(projection='3d')
-    ax._axis3don = False
-    plt.legend(labels)
+        training_set, test_set, full_set = loadin.main()
+        chn = full_set.shape[2]
+
+        outputdict = {}
+
+        for hidden_size in hidden_list:
+            for_tsne = []
+            label_list = []
+            rnn = torch.load('Trained_Model/trained_model_' + str(hidden_size) + '_hl.out')
+            for label in range(len(labels)):
+                for per in range(full_set.shape[1]):
+                    for ex in range(full_set.shape[3]):
+                        data = full_set[label, per, :, ex, :]
+                        input_data = Variable(making_tensor(data, chn))
+                        output = evaluate(input_data)
+                        ts_fill = output.data[0].numpy()
+                        # ts_fill = ts_fill / np.mean(ts_fill)
+                        for_tsne.append(ts_fill)
+                        label_list.append(label)
+            tsne_input = np.stack(for_tsne, axis=0)
+            tsne_input = tsne_input / np.mean(tsne_input)
+            label_color = np.array(label_list)
+            tsne_embedded = tsne.fit_transform(tsne_input)
+            outputdict[str(hidden_size)] = tsne_embedded
+        outputdict['Label Color'] = label_color
+        outputdict['Size Set'] = 900
+        with open(file_des, 'wb') as handle:
+            pickle.dump(outputdict, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        print('TSNE Data Saved')
+    else:
+        with open(file_des, 'rb') as handle:
+            outputdict = pickle.load(handle)
+        print('TSNE Data Loaded')
+    int_amount = 30
+    int_dict = interpolation(outputdict, hidden_list, int_amount)
+    fig = plt.figure()
+    ax = p3.Axes3D(fig)
+    com = '0'
+    ax.set_xlim3d([-15, 15])
+
+    ax.set_ylim3d([-15, 15])
+
+    ax.set_zlim3d([-15, 15])
+    ax.set_title('tSNE of RNN with ' + str(hidden_list[0]) + ' size for hidden layer', y=1)
+
+    ob = ax.scatter(int_dict[com][:, 0], int_dict[com][:, 1], int_dict[com][:, 2], c=outputdict['Label Color'])
+
+    ani = animation.FuncAnimation(fig, update, int_amount * len(hidden_list),
+                                  fargs=[int_dict, ob, hidden_list, int_amount], interval=1,
+                                  blit=False)
+
+    plt.axis('off')
     plt.show()
-
-
-
